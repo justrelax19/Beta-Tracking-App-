974323507d8b30322532e45e68af8a9792cc4676
justrelax19 <vaibhav19.garg@gmail.com>
2017-01-17 22:14:15 +0530

    First commit of beta version

---
+++ app/src/main/java/info/androidhive/materialdesign/FButton.java
@@ -0,0 +1,14 @@
+package info.androidhive.materialdesign;
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.View;
+
+import info.androidhive.materialdesign.activity.R;
+
+/**
+ * Created by Anku on 10/7/2016.
+ */
+public class FButton {
+
+}
---
+++ app/src/main/java/info/androidhive/materialdesign/activity/MapsActivity.java
@@ -0,0 +1,326 @@
+package info.androidhive.materialdesign.activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+
+public class MapsActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener
+         {
+
+    //Our Map
+    private GoogleMap mMap;
+             private BoomMenuButton bmb;
+    //To store longitude and latitude from map
+    public double longitude;
+    public double latitude;
+             private static int[] imageResources = new int[]{
+                     R.drawable.friends,
+                     R.drawable.messages,
+                     R.drawable.about,
+                     R.drawable.signout,
+             };
+             private static String[] stringResources = new String[]{
+                     "Friends",
+                     "Message",
+                     "About Us",
+                     "Sign Out",
+             };
+             private static int imageResourceIndex = 0;
+             private static int stringResourceIndex = 0;
+
+    //Buttons
+   // private ImageButton buttonSave;
+   // private ImageButton buttonCurrent;
+   // private ImageButton buttonView;
+
+    //Google ApiClient
+    private GoogleApiClient googleApiClient;
+
+             static int getImageResource() {
+                 if (imageResourceIndex >= imageResources.length) imageResourceIndex = 0;
+                 return imageResources[imageResourceIndex++];
+             }
+
+             static String getStringResource() {
+                 if (stringResourceIndex >= stringResources.length) stringResourceIndex = 0;
+                 return stringResources[stringResourceIndex++];
+             }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        bmb = (BoomMenuButton) findViewById(R.id.bmb);
+
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
+                    .listener(new OnBMClickListener() {
+                        @Override
+                        public void onBoomButtonClick(int index) {
+                            Log.e("listener","called "+index+"");
+                        }
+                    })
+                    .normalImageRes(getImageResource())
+                    .normalText(String.valueOf(getStringResource()));
+            bmb.addBuilder(builder);
+        }
+
+
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //Initializing googleapi client
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+
+
+    }
+      //  moveMap();
+
+        //Initializing views and adding onclick listeners
+       // buttonSave = (ImageButton) findViewById(R.id.buttonSave);
+    //    buttonCurrent = (ImageButton) findViewById(R.id.buttonCurrent);
+       // buttonView = (ImageButton) findViewById(R.id.buttonView);
+       // buttonSave.setOnClickListener(this);
+  //      buttonCurrent.setOnClickListener(this);
+       // buttonView.setOnClickListener(this);
+
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+
+        super.onStart();
+        startThread();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    //Getting current location
+    public void getCurrentLocation() {
+        mMap.clear();
+        //Creating a location object
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (location != null) {
+            //Getting longitude and latitude
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+   mMap.animateCamera(CameraUpdateFactory.zoomTo(40));
+
+            //moving the map to location
+            moveMap();
+
+        }
+    }
+
+
+
+    //Function to move the map
+    public void moveMap() {
+        //String to display current latitude and longitude
+        String msg = latitude + ", "+longitude;
+
+        //Creating a LatLng Object to store Coordinates
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        //Adding marker to map
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng) //setting position
+                .draggable(true) //Making the marker draggable
+                .title("Current Location")); //Adding a title
+
+        //Moving the camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        //Animating the camera
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        //Displaying current coordinates in toast
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng latLng = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        //Clearing all the markers
+        mMap.clear();
+
+        //Adding a new marker to the current pressed position
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true));
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        //Getting the coordinates
+        latitude = marker.getPosition().latitude;
+        longitude = marker.getPosition().longitude;
+
+        //Moving the map
+        moveMap();
+
+
+    }
+
+
+            //getCurrentLocation();
+            //new updatemymapp().execute();
+
+
+
+    void startThread()
+    {
+        Thread th=new Thread(){
+
+            @Override
+            public void run(){
+                //
+                try
+                {
+                    for (;;)
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+
+
+                                getCurrentLocation();
+
+
+
+                                }
+                                catch(Exception e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        Thread.sleep(2000);
+
+                    }
+                }
+                catch (InterruptedException e) {
+                }
+
+            }
+        };
+        th.start();
+    }
+
+
+
+
+
+}
+
+/*
+class updatemymapp extends AsyncTask<Void,Void,Void>
+{
+    MapsActivity ma = new MapsActivity();
+
+
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        int t=200;
+        while(t!=0) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ma.getCurrentLocation();
+        */
+/*    ma.latitude = marker.getPosition().latitude;
+            ma.longitude = marker.getPosition().longitude;
+            ma.moveMap();*//*
+
+            t--;
+        }
+        return null;
+    }
+}*/
+
